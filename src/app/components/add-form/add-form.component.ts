4a52b779b7826478c4e3b4fbbd837ad0716715c6
dushka-dragoeva <d.dragoeva@abv.bg>
2017-01-02 08:18:07 +0200

    Input data reached listings.component.ts

--- src/app/components/add-form/add-form.component.ts
+++ src/app/components/add-form/add-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ItemListing } from '../../models/item.listing.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
@@ -9,26 +11,61 @@ import { Component, Input } from '@angular/core';
 
 export class AddFormComponent {
 
-  listingItemForm: any;
-
-  constructor() {
-    this.listingItemForm = {
+  listingItemForm: ItemListing = {
       title: '',
       category: 'Tърси',
-      postedOn: new Date().toLocaleTimeString(),
-      author: 'Pesjo',
       content: '',
+      username: 'Pesjo',
+      date: new Date().toLocaleTimeString(),
       isDeleted: false
     };
+  @Output() listingItemCreated = new EventEmitter();
+createListingItem() {
+    const {title, category, content, username, date, isDeleted} = this.listingItemForm;
+     if (title && category && content && username && date){
+      this.listingItemCreated.next({title, category, content, username, date, isDeleted});
+      this.listingItemForm.title= '',
+      this.listingItemForm.category= 'Tърси',
+      this.listingItemForm.content= '',
+      this.listingItemForm.username= 'Pesjo',
+      this.listingItemForm.date= new Date().toLocaleTimeString(),
+      this.listingItemForm.isDeleted=false;
+     }
   }
+
+  // constructor() {
+  //   this.listingItemForm = {
+  //     title: '',
+  //     category: 'Tърси',
+  //     content: '',
+  //     username: 'Pesjo',
+  //     date: new Date().toLocaleTimeString(),
+  //     isDeleted: false
+  //   };
+  // }
+  // @Input
+
   isFormVisible: boolean = false;
 
   toggleForm() {
     this.isFormVisible = !this.isFormVisible;
   }
 
- get spy(){
+  get spy() {
     return JSON.stringify(this.listingItemForm);
   }
+
+  // newListingItemForm = {
+  //   title: '',
+  //   category: 'Tърси',
+  //   postedOn: new Date().toLocaleTimeString(),
+  //   author: 'Pesjo',
+  //   content: '',
+  //   isDeleted: false
+  // }
+
+
+
+  
 
 }
--- src/app/components/listings/listings.component.ts
+++ src/app/components/listings/listings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { DataService } from '../../services/data.service';
-import { ForumPost } from '../forum/forum-elements/forum.post';
 import { AddFormComponent } from '../add-form/add-form.component';
 import { ListingItemComponent } from '../listing-item/listing-item.component';
 import { ItemListing } from '../../models/item.listing.model'
@@ -15,13 +16,39 @@ import { ItemListing } from '../../models/item.listing.model'
 export class ListingsComponent implements OnInit {
   listingItem: ItemListing;
   listings: ItemListing[] = [{
-    title: '',
-    category: 'Търси',
-    content: 'Alalalal',
-    username: 'Анонимен',
-    date: '',
-    isDeleted: false
-  }];
+      
+        title: 'Спешно za 5 god.  dete',
+        content: `Агенция за детегледачи/чки "АБЕЦЕ Комюникейшън" търси за свои клиенти, 
+        живеещи в гр. София, кв. Слатина, Детегледач/ка / домашен/на помощник/ца - почасови грижи 
+        за дете на 1 г. и 4 месеца и помощ в домакинството.`,
+		 category: 'Търси',
+        username: 'Anonim1',
+        date: '123',
+		 isDeleted: false
+      },
+
+      {
+        title: 'Спешно za 2 god.  dete',
+        content: `Агенция за детегледачи/чки "АБЕЦЕ Комюникейшън" търси за свои клиенти, 
+        живеещи в гр. София, кв. Слатина, Детегледач/ка / домашен/на помощник/ца - почасови грижи 
+        за дете на 1 г. и 4 месеца и помощ в домакинството.`,
+        category: 'Търси',
+	   username: 'Anonim2',
+        date: '1234',
+		 isDeleted: false
+      },
+
+      {
+        title: 'Спешно za 12 god.  dete',
+        content: `Агенция за детегледачи/чки "АБЕЦЕ Комюникейшън" търси за свои клиенти, 
+        живеещи в гр. София, кв. Слатина, Детегледач/ка / домашен/на помощник/ца - почасови грижи 
+        за дете на 1 г. и 4 месеца и помощ в домакинството.`,
+ category: 'Търси',
+        username: 'Anonim3',
+        date: '27/0302',
+		 isDeleted: false
+      }];
+  
   title: string;
   category: string;
   content: string;
@@ -32,7 +59,7 @@ export class ListingsComponent implements OnInit {
 
 
   constructor(private dataService: DataService) {
-    this.dataService.getListings().subscribe(listings => { this.listings = listings });
+  //  this.dataService.getListings().subscribe(listings => { this.listings = listings });
     // todo same with user
     this.listingItem = {
       title: '',
@@ -60,6 +87,12 @@ export class ListingsComponent implements OnInit {
     this.isFormVisible = !this.isFormVisible;
   }
 
+
+  createListingItem(listingItem:ItemListing) {
+    console.log(listingItem);
+    this.listings.push(listingItem)
+  }
+
   addItemListing(): void {
     let newItemListing = {
       title: this.listingItem.title,
@@ -72,16 +105,14 @@ export class ListingsComponent implements OnInit {
     console.log('addingItem');
     console.log(JSON.stringify(newItemListing));
 
-    this.dataService.addListingItem(newItemListing).subscribe(listingItem => {
-      this.listings.push(listingItem);
-      this.title = '',
-        this.category = 'Търси',
-        this.content = '',
-        this.username = 'Анонимен',
-        this.date = '',
-        this.isDeleted = false
-
-    })
-
+    // this.dataService.addListingItem(newItemListing).subscribe(listingItem => {
+    //   this.listings.push(listingItem);
+    //   this.title = '',
+    //     this.category = 'Търси',
+    //     this.content = '',
+    //     this.username = 'Анонимен',
+    //     this.date = '',
+    //     this.isDeleted = false
+    // })
   }
 }
