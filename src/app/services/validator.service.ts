56efb4f603b45944eeb1c2e1f52c87a88c5e5fe7
Absend <at.papucharov@gmail.com>
2017-01-02 10:27:04 +0200

    validator and notifications added

---
+++ src/app/services/validator.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ValidatorService {
+
+	constructor() { }
+
+	stringNotInRange(value: string, min: number, max: number): boolean {
+		if (value.length < min || value.length > max) {
+			return true;
+		}
+		return false;
+	}
+
+	stringNotContainIllicitChars(value: string): boolean {
+		for (let ch of value) {
+			if (ch === '@' || ch === '#' || ch === '!' || ch === '?') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	stringSame(p: string, pC: string): boolean {
+		if (p !== pC) {
+			return false;
+		}
+		return true;
+	}
+
+	stringExistsInCollection(collection: string[], value: string) {
+		if (collection.indexOf(value) !== -1) {
+			return true;
+		}
+		return false;
+	}
+}
